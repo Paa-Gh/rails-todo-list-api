56bd2cfdd6b031d037924fb98d8fe918cfa2a1e5
Paa-Gh <67100385+Paa-Gh@users.noreply.github.com>
2020-10-04 23:39:39 +0000

    first commit

---
+++ app/serializers/todo_serializer.rb
@@ -0,0 +1,8 @@
+class TodoSerializer < ActiveModel::Serializer
+  attributes :id, :title, :items
+  def items
+    self.object.items.map do |item|
+      {id:item.id, name:item.name, done:item.done}
+    end
+  end
+end
